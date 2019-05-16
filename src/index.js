e451e104d8ae7876ec914ba3d08593d77cb94652
weizheng pan <noone@weizhengdeMacBook-Pro.local>
2019-05-16 15:26:14 +0800

    feat: add between notbetween expire ipAllow ipDeny

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // [验证字段1,验证规则,错误提示,[验证条件,附加规则]],
 
 // 验证规则
-// require 字段必须、email 邮箱、url URL地址、number 数字
+// require 字段必须、email 邮箱、url URL地址、number 数字、手机号码 phone
 
 // 验证条件 （可选）
 // 包含下面几种情况：
@@ -17,30 +17,29 @@
 // confirm	  验证表单中的两个字段是否相同，定义的验证规则是一个字段名
 
 // equal     	验证是否等于某个值，该值由前面的验证规则定义
-// notequal	  验证是否不等于某个值，该值由前面的验证规则定义
+// notEqual	  验证是否不等于某个值，该值由前面的验证规则定义
 // in	        验证是否在某个范围内，定义的验证规则可以是一个数组或者逗号分割的字符串
-// notin	    验证是否不在某个范围内，定义的验证规则可以是一个数组或者逗号分割的字符串（3.1.2版本新增）
+// notIn	    验证是否不在某个范围内，定义的验证规则可以是一个数组或者逗号分割的字符串（3.1.2版本新增）
 // length	    验证长度，定义的验证规则可以是一个数字（表示固定长度）或者数字范围（例如3,12 表示长度从3到12的范围）
 
-// TODO:
 // between	  验证范围，定义的验证规则表示范围，可以使用字符串或者数组，例如1,31或者array(1,31)
-// notbetween	验证不在某个范围，定义的验证规则表示范围，可以使用字符串或者数组
+// notBetween	验证不在某个范围，定义的验证规则表示范围，可以使用字符串或者数组
 // expire	    验证是否在有效期，定义的验证规则表示时间范围，可以到时间，例如可以使用 2012-1-15,2013-1-15 表示当前提交有效期在2012-1-15到2013-1-15之间，也可以使用时间戳定义
-// ip_allow	  验证IP是否允许，定义的验证规则表示允许的IP地址列表，用逗号分隔，例如201.12.2.5,201.12.2.6
-// ip_deny	  验证IP是否禁止，定义的验证规则表示禁止的ip地址列表，用逗号分隔，例如201.12.2.5,201.12.2.6
+// ipAllow	  验证IP是否允许，定义的验证规则表示允许的IP地址列表，用逗号分隔，例如201.12.2.5,201.12.2.6
+// ipDeny	  验证IP是否禁止，定义的验证规则表示禁止的ip地址列表，用逗号分隔，例如201.12.2.5,201.12.2.6
 
 // 是否拥有该字段
 const hasOwnProperty = (obj, key) => obj.hasOwnProperty(key)
 
 // 判断不为空
-const isNoNull =(val) => val !== undefined && val !== null && val !== ''
+const isNoNull = (val) => val !== undefined && val !== null && val !== ''
 
 export default {
   data: {},
   error: {},
   // 设置数据
   setData(formData) {
-    if(typeof formData !== 'object') {
+    if (typeof formData !== 'object') {
       return new Error('data must be object')
     }
     this.data = Object.assign({}, formData)
@@ -51,7 +50,7 @@ export default {
   validate(rules) {
     let result = true // 返回结果 默认是通过
     rules.forEach(rule => {
-      const [fieldName, , errorMessage] = rule
+      const [fieldName,, errorMessage] = rule
       // 如果错误
       if (!this._validateByRuleOne(rule)) {
         // 目前只用判断第一个..
@@ -86,7 +85,7 @@ export default {
   },
   // 验证条件
   _validateByCondition(rule = []) {
-    let [fieldName, , , condition = 0] = rule;
+    let [fieldName,,, condition = 0] = rule
     // 如果有字段，进行校验， 没有允许通过
     if (condition === 0) {
       if (hasOwnProperty(this.data, fieldName)) {
@@ -114,7 +113,7 @@ export default {
   },
   // 检测验证条件
   _validateByCustomRuleOrOtherRule(rule) {
-    let [, , , , otherRule] = rule;
+    let [,,,, otherRule] = rule
     if (otherRule) {
       return this._validateByOtherRule(rule)
     } else {
@@ -123,7 +122,7 @@ export default {
   },
   // 使用默认的条件 如require
   _validateByCustomRule(rule) {
-    let [, customRule] = rule;
+    let [, customRule] = rule
     switch (customRule) {
       case 'require':
         return this.handleCustomRuleByRequire(rule)
@@ -133,11 +132,13 @@ export default {
         return this.handleCustomRuleByUrl(rule)
       case 'number':
         return this.handleCustomRuleByNumber(rule)
+      case 'phone':
+        return this.handleCustomRuleByPhone(rule)
     }
   },
   // 使用自定义规则 如regex
   _validateByOtherRule(rule) {
-    let [, , , , otherRule] = rule;
+    let [,,,, otherRule] = rule
     // 这里上层其实拦截了。 不过再做一层 增加健壮性
     if (!otherRule) return false
     if (otherRule instanceof RegExp) {
@@ -160,12 +161,22 @@ export default {
           return !this.handleOtherRuleByIn(rule)
         case 'length':
           return this.handleOtherRuleByLength(rule)
+        case 'between':
+          return this.handleOtherRuleByBetween(rule)
+        case 'notBetween':
+          return !this.handleOtherRuleByBetween(rule)
+        case 'expire':
+          return this.handleOtherRuleByExpire(rule)
+        case 'ipAllow':
+          return this.handleOtherRuleByIpAllow(rule)
+        case 'ipDeny':
+          return !this.handleOtherRuleByIpAllow(rule)
       }
     }
   },
   // require 默认提供条件
   handleCustomRuleByRequire(rule) {
-    let [fieldName] = rule;
+    let [fieldName] = rule
     let ret = true
     // 如果不存在就为false
     if (ret && !hasOwnProperty(this.data, fieldName)) {
@@ -177,10 +188,19 @@ export default {
     }
     return ret
   },
+  // phone 默认提供条件
+  handleCustomRuleByPhone(rule) {
+    let [fieldName] = rule
+    // 如果不存在就为false
+    if (!hasOwnProperty(this.data, fieldName)) {
+      return false
+    }
+    return /^[1][3,4,5,7,8][0-9]{9}$/.test(this.data[fieldName])
+  },
   // email 默认提供条件
   handleCustomRuleByEmail(rule) {
-    var emailReg = /^[-_A-Za-z0-9]+@([_A-Za-z0-9]+\.)+[A-Za-z0-9]{2,3}$/;
-    let [fieldName] = rule;
+    var emailReg = /^[-_A-Za-z0-9]+@([_A-Za-z0-9]+\.)+[A-Za-z0-9]{2,3}$/
+    let [fieldName] = rule
     // 检验是否存在这个数据
     let ret = this.handleCustomRuleByRequire(rule)
     if (ret && !emailReg.test(this.data[fieldName])) {
@@ -192,10 +212,10 @@ export default {
   handleCustomRuleByUrl(rule) {
     // TODO: 感觉这个正则不是特别好
     // http2://www.baidu.com 这种都能通过... 
-    function isNoURL(str) {
-      return !str.match(/(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g);
+    function isNoURL (str) {
+      return !str.match(/(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g)
     }
-    let [fieldName] = rule;
+    let [fieldName] = rule
     // 检验是否存在这个数据
     let ret = this.handleCustomRuleByRequire(rule)
     if (ret && isNoURL(this.data[fieldName])) {
@@ -205,7 +225,7 @@ export default {
   },
   // number 默认提供条件
   handleCustomRuleByNumber(rule) {
-    let [fieldName] = rule;
+    let [fieldName] = rule
     // 检验是否存在这个数据
     let ret = this.handleCustomRuleByRequire(rule)
     if (ret && (typeof (this.data[fieldName]) !== 'number')) {
@@ -215,7 +235,7 @@ export default {
   },
   // reg 附加条件
   handleOtherRuleByReg(rule) {
-    let [fieldName, customRule, errorMessage, condition = 0, otherRule] = rule;
+    let [fieldName, customRule, errorMessage, condition = 0, otherRule] = rule
     // 检验是否存在这个数据
     let ret = this.handleCustomRuleByRequire(rule)
     if (ret && !otherRule.test(this.data[fieldName])) {
@@ -225,7 +245,7 @@ export default {
   },
   // function 附加条件
   handleOtherRuleByFunc(rule) {
-    let [fieldName, customRule, errorMessage, condition = 0, otherRule] = rule;
+    let [fieldName, customRule, errorMessage, condition = 0, otherRule] = rule
     // 检验是否存在这个数据
     let ret = this.handleCustomRuleByRequire(rule)
     if (ret && !otherRule(this.data[fieldName])) {
@@ -235,7 +255,7 @@ export default {
   },
   // equal 附加条件
   handleOtherRuleByEqual(rule) {
-    let [fieldName1, val, errorMessage, condition = 0, otherRule] = rule;
+    let [fieldName1, val, errorMessage, condition = 0, otherRule] = rule
     // 检验是否存在这个数据
     const val1 = isNoNull(this.data[fieldName1])
     const val2 = isNoNull(val)
@@ -246,7 +266,7 @@ export default {
   },
   // confirm 附加条件
   handleOtherRuleByConfirm(rule) {
-    let [fieldName1, fieldName2, errorMessage, condition = 0, otherRule] = rule;
+    let [fieldName1, fieldName2, errorMessage, condition = 0, otherRule] = rule
     // 检验是否存在这个数据
     const val1 = isNoNull(this.data[fieldName1])
     const val2 = isNoNull(this.data[fieldName2])
@@ -257,7 +277,7 @@ export default {
   },
   // in 附加条件
   handleOtherRuleByIn(rule) {
-    let [fieldName1, rangs, errorMessage, condition = 0, otherRule] = rule;
+    let [fieldName1, rangs, errorMessage, condition = 0, otherRule] = rule
     // 检验是否存在这个数据
     const val1 = isNoNull(this.data[fieldName1])
     const val2 = isNoNull(rangs)
@@ -268,11 +288,73 @@ export default {
   },
   // length 附加条件
   handleOtherRuleByLength(rule) {
-    let [fieldName1, len, errorMessage, condition = 0, otherRule] = rule;
+    let [fieldName1, len, errorMessage, condition = 0, otherRule] = rule
     // 检验是否存在这个数据
     const val1 = isNoNull(this.data[fieldName1])
     if (val1 && +len === String(this.data[fieldName1]).length) {
       return true
+    }
+    return false
+  },
+  // between 附加条件
+  handleOtherRuleByBetween(rule) {
+    let [fieldName1, val, errorMessage, condition = 0, otherRule] = rule
+    // 检验是否存在这个数据
+    const val1 = isNoNull(this.data[fieldName1])
+    const val2 = isNoNull(val)
+    if (val1 && val2) {
+      const [start, end] = val.split(',')
+      if (val1 >= start && val1 <= end) {
+        return true
+      }
+    }
+    return false
+  },
+  // expire 附加条件
+  handleOtherRuleByExpire(rule) {
+    let [fieldName1, val, errorMessage, condition = 0, otherRule] = rule
+    // 检验是否存在这个数据
+    let val1 = isNoNull(this.data[fieldName1])
+    let val2 = isNoNull(val)
+    if (val1 && val2) {
+      val1 = String(val1)
+      // 如果是 2018-5-11 格式
+      if (val.includes('-') || val.includes('/')) {
+        val1 = this.data[fieldName1].replace(/\-/g, '/')
+        val = val.replace(/\-/g, '/')
+        let [startTime, endTime] = val.split(',')
+        startTime = (+new Date(startTime))
+        endTime = (+new Date(endTime))
+        val1 = (+new Date(val1))
+        if (isNaN(startTime) || isNaN(endTime)) {
+          console.error(`\r\nPlease check ${fieldName1} conditions, maybe the date is wrong 📅 \r\n`)
+          return false
+        }
+        if (val1 >= startTime && val1 <= endTime) {
+          return true
+        }
+      } else {
+        // 时间戳格式 规定如果是时间戳全部都得是时间戳
+        const [startTime, endTime] = val.split(',')
+        const v = this.data[fieldName1]
+        if (v >= startTime && v <= endTime) {
+          return true
+        }
+      }
+    }
+    return false
+  },
+  // ipAllow 附加条件
+  handleOtherRuleByIpAllow(rule) {
+    let [fieldName1, val, errorMessage, condition = 0, otherRule] = rule
+    // 检验是否存在这个数据
+    let val1 = isNoNull(this.data[fieldName1])
+    let val2 = isNoNull(val)
+    if (val1 && val2) {
+      const ips = val.split(',')
+      if (ips.includes(this.data[fieldName1])) {
+        return true
+      }
     }
     return false
   }
