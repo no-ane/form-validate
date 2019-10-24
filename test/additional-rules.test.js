46bfc8533e923d9e0b79b522fc21d4498a5eed01
panweizheng <noone@weizhengdeMacBook-Pro.local>
2019-10-24 10:49:12 +0800

    feat: unit test

---
+++ test/additional-rules.test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const validator = require('../index').default;
+
+describe('additional-rules', function () {
+  it('diff error', function () {
+    const formData = {
+      name: '小明',
+    }
+    const rules = [
+      ['name', '小明', 'error message', 1, (data, val) => val === data]
+    ]
+    assert.strictEqual(validator.setData(formData).validate(rules), true);
+  });
+
+  it('error message', function () {
+    const formData = {
+      name: '小明',
+    }
+    const rules = [
+      ['name', '小李', 'error message', 1, (data, val) => val === data],
+    ]
+    assert.strictEqual(validator.setData(formData).validate(rules), false);
+    assert.strictEqual(Object.values(validator.getError()).join(), 'error message');
+  });
+
+  it('name diff', function () {
+    const formData = {
+      name: '小明',
+    }
+    const rules = [
+      ['mingzi', '小李', 'error message', 1, (data, val) => val === data],
+    ]
+    assert.strictEqual(validator.setData(formData).validate(rules), false);
+    assert.strictEqual(Object.values(validator.getError()).join(), 'error message');
+  });
+
+});
---
+++ test/validate-type.test.js
@@ -0,0 +1,101 @@
+const assert = require('assert');
+const validator = require('../index').default;
+
+describe('validate-type', function () {
+  it('init validate type', function () {
+    {
+      const formData = {
+        name: '小明',
+      }
+      const rules = [
+        ['age', 'require', 'error message']
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), true);
+    }
+
+    {
+      const formData = {
+        name: '小明',
+      }
+      const rules = [
+        ['name', 'xiao li', 'The name must be xiao li', 0, (val, secondVal) => val === secondVal]
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), false);
+      assert.strictEqual(Object.values(validator.getError()).join(), 'The name must be xiao li');
+    }
+  });
+
+  it('1 validate type', function () {
+    {
+      const formData = {
+        name: '小明',
+      }
+      const rules = [
+        ['name', 'require', 'The name can not be empty when the validate type is 1', 1],
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), true);
+      assert.strictEqual(Object.values(validator.getError()).join(), '');
+    }
+
+    {
+      const formData = {
+        age: 18,
+      }
+      const rules = [
+        ['name', 'require', 'The name is empty.', 1],
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), false);
+      assert.strictEqual(Object.values(validator.getError()).join(), 'The name is empty.');
+    }
+
+    {
+      const formData = {
+        age: 18,
+      }
+      const rules = [
+        ['name', 'require', 'The name can not be empty..', 1],
+        ['age', 'number', 'The age must be 18', 1],
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), false);
+      assert.strictEqual(Object.values(validator.getError()).join(), 'The name can not be empty..');
+    }
+  });
+
+  it('2 validate type', function () {
+    {
+      const formData = {
+        name: '小明',
+      }
+      const rules = [
+        ['age', 'number', 'The age can be empty.', 2],
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), true);
+      assert.strictEqual(Object.values(validator.getError()).join(), '');
+    }
+
+    {
+      const formData = {
+        name: '小李',
+        age: 18,
+      }
+      const rules = [
+        ['name', 'number', 'The name must be a number..', 2],
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), false);
+      assert.strictEqual(Object.values(validator.getError()).join(), 'The name must be a number..');
+    }
+
+    {
+      const formData = {
+        name: '小李',
+        age: 18,
+      }
+      const rules = [
+        ['name', 'require', 'no error message', 2],
+        ['age', 'number', 'The age must be a number..', 2],
+      ]
+      assert.strictEqual(validator.setData(formData).validate(rules), true);
+      assert.strictEqual(Object.values(validator.getError()).join(''), '');
+    }
+  });
+});
